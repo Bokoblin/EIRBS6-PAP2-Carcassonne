5ef3419bc0b41ea393b1aab40abe980b51b17cd7
broseau <broseau@enseirb-matmeca.fr>
2018-03-23 14:25:15 +0000

    Ajout d'un début de server.c pour faire un test.

---
+++ src/server/server.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dlfcn.h>
+#include "../common/projCarcassonneInterface.h"
+
+int main(int argc, char **argv)
+{
+  void *handle;
+  char const* (*get_client1_name)(void);
+  char *error;
+  
+  handle = dlopen ("../install/client1.so", RTLD_NOW);
+  /* If an error occured */
+  if (!handle) {
+    fputs (dlerror(), stderr);
+    exit(1);
+  }
+  
+  get_client1_name = dlsym(handle, "get_player_name");
+  /* If the symbol searched wasn't found */
+  if ((error = dlerror()) != NULL)  {
+    fputs(error, stderr);
+    exit(1);
+  }
+
+
+  /* TEST ZONE !!*/
+  char const *player_name = get_client1_name();
+  printf("%s", player_name);
+  
+  dlclose(handle);
+  return EXIT_SUCCESS;
+}
