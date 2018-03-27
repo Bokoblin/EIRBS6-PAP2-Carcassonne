8330dab461fef0aedb38d8bec55bd8b2c80ea037
hbenabdelje <hbenabdelje@enseirb-matmeca.fr>
2018-03-27 12:41:09 +0000

    moidification client1.c

--- src/client/client1.c
+++ src/client/client1.c
@@ -7,21 +7,26 @@
 
 char const* get_player_name()
 {
+  /*
   char* name;
   name = (char*) malloc(20 * sizeof(char));
   printf("Entrez le nom du joueur:\n");
   scanf("%s", name);
   return name;
+  */
+  return("Joueur1");
 }
 
 void initialize(unsigned int id, unsigned int n_players)
 {
-    //TODO: Is it a client function?
+    //TODO: Is it a client function? OUI TOUTES LES FONCTIONS SONT IMPLÉMENTÉES PAR LE CLIENTS
+  printf("initialise fonctionne pour %s\n", get_player_name());
 }
 
 struct move play(enum card_id card, struct move const previous_moves[], size_t n_moves)
 {
     //TODO
+   printf("play fonctionne pour %s\n", get_player_name());
 };
 
 /* Announces the end of the game to the player, and cleans up the
@@ -33,6 +38,7 @@ struct move play(enum card_id card, struct move const previous_moves[], size_t n
 void finalize()
 {
     //TODO: Is it a client function?
+   printf("finalize fonctionne pour %s\n", get_player_name());
 }
 
 int main()
