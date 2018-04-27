c99d76c37f5428d3b9a9333ccf25bfb0a58fe379
broseau <broseau@enseirb-matmeca.fr>
2018-04-27 13:07:07 +0000

    added client_functions.c

---
+++ src/client/client_functions.c
@@ -0,0 +1,9 @@
+#include "../common/common_interface.h"
+#include "client.h"
+
+
+/////////////////////////////////////////////////////////
+/// Implementation of client usefull functions
+/////////////////////////////////////////////////////////
+
+char *ask_for_player_name(struct client *client);
