854373be85572cb5f05a3bbfd7577d7c7871d34a
broseau <broseau@enseirb-matmeca.fr>
2018-05-11 15:17:00 +0000

    Added zone.c and zone.h

---
+++ src/common/zone.h
@@ -0,0 +1,102 @@
+#ifndef ZONE_H
+#define ZONE_H
+
+#include "ADT/set.h"
+#include "card.h"
+#include "meeple.h"
+#include "area_type.h"
+
+/**
+ * @brief A structure containg every zones (roads, cities etc..)
+ * 
+ */
+struct zone
+{
+struct set *area_set;
+struct set *meeple_set;
+enum area_type;
+unsigned int score;
+unsigned int holes;
+};
+
+/**
+ * @brief Creates and initialise a new zone
+ * 
+ * @param area_type Is the area type of the zone
+ * @return struct zone* 
+ */
+struct zone *empty_zone(enum area_type);
+
+
+/**
+ * @brief Returns a set of pointors to enum place*
+ * Only one enum place by zone is required
+ * (advice: use the allowed_positions)
+ * 
+ * @param c is the considered card
+ * @return struct set* of enum place*
+ */
+struct set *zone__cards_available_zones(struct card *c);
+
+
+/**
+ * @brief Returns the set containing the given area
+ * 
+ * @param area_type is a pointor to the area in the enum area_type areas[MAX_ZONES] in the card
+ * @return struct set* containg the area
+ */
+struct set *zone__matching_area_set(enum area_type *area);
+
+
+/**
+ * @brief Adds all the areas on the card that makes a same zone
+ * Also update the score and the holes numbers on the given struct zone
+ * 
+ * @param z in where you want to add the areas
+ * @param c is the card containing the areas
+ * @param place is one of the areas making a zone
+ * @return SUCCESS if the areas were added, FAILURE else
+ */
+int zone__add_areas(struct zone *z, struct card *c, enum place);
+
+
+/**
+ * @brief Verify if a fusion is required
+ * 
+ * Verify on each neighboor of the card if two differents zones are linked.
+ * If yes, returns a set of zones that have to be fusioned
+ * 
+ * @param c is the card you have to check the neighboor from
+ * @return int: 1 if the fusion is required, 0 if not
+ */
+int *zone__is_fusion_required(struct card *c);
+
+
+/**
+ * @brief Make a fusion of z1 and z2
+ * 
+ * @return a new zone which is the fusion of z1 and z2
+ */
+struct zone *zone__fusion_zone(struct zone *z1, struct zone *z2);
+
+
+int zone__update_();
+
+/**
+ * @brief Adds a meeple to the zone
+ * 
+ * @param z is the zone where you want to place a meeple
+ * @param m is the already-initialised meeple
+ * @return SUCCESS if the areas were added, FAILURE else
+ */
+int zone__add_meeple(struct zone *z, struct meeple *m);
+
+
+/**
+ * @brief Frees all the allocated memory in the zone
+ * 
+ * @param z is the zone you have to free
+ */
+void zone_free(struct zone *z);
+
+#endif 
---
+++ tst/test_client.c
@@ -0,0 +1,96 @@
+#include "common_tests_utils.h"
+#include "../src/common/utils.h"
+#include "../src/client/client.h"
+#include "../src/common/card.h"
+
+
+int test_client__client_init()
+{
+    printf("%s... ", __func__);
+
+    struct client cli;
+    client__init(&cli, 4, 2);
+
+    if ((cli.id == 4)
+     && (cli.nb_players == 2)
+     && (cli.nb_meeples == MAX_MEEPLES)
+     && (set__size(cli.client_board->cards_set) == 1)
+     && (cli.client_board->first_card != NULL)
+     && (cli.client_board->first_card->type.id == CARD_JUNCTION_CITY)) {
+        client__free(&cli);
+        return TEST_SUCCESS;
+    }
+
+    client__free(&cli);
+
+    return !TEST_SUCCESS;
+}
+
+
+int test_client__client_update_board()
+{
+    printf("%s... ", __func__);
+
+    struct client cl;
+    client__init(&cl, 4, 2);
+
+//    struct board *b = board__init();
+//    enum card_id ci = CARD_CITY_ALL_SIDES;
+//    stack__push(b->drawing_stack, &ci);
+//    board__init_first_card(b);
+//    board__free(cl.client_board);
+//    cl.client_board = b;
+
+    struct move const mv1 = {VALID, 1, CARD_CITY_TUNNEL, {0, 1}, NORTH, NO_MEEPLE};
+    struct move const mv2 = {VALID, 2, CARD_PLAIN_TWO_CITIES, {0, -1}, NORTH, NO_MEEPLE};
+    struct move const previous_moves[] = {mv1, mv2};
+
+    client__update_board(&cl, previous_moves, 2);
+
+    set__debug_data(cl.client_board->cards_set, false);
+    if (set__size(cl.client_board->cards_set) != 3) { //FIXME: Only 1 card out of 2 added
+        client__free(&cl);
+        return !TEST_SUCCESS;
+    }
+
+    client__free(&cl);
+
+    return TEST_SUCCESS;
+}
+
+
+int test_client__client_play_card_success_case()
+{
+    printf("%s... ", __func__);
+
+    struct client cli;
+    client__init(&cli, 4, 2);
+
+    struct move mv = client__play_card(&cli, CARD_ROAD_TURN_LEFT_CITY);
+
+    if (mv.check == VALID) {
+        client__free(&cli);
+        return TEST_SUCCESS;
+    }
+
+    client__free(&cli);
+
+    return !TEST_SUCCESS;
+}
+
+
+int main()
+{
+    printf("----------- TEST CLIENT -----------\n");
+
+    int nb_success = 0;
+    int nb_tests = 0;
+
+    print_test_result(test_client__client_init(), &nb_success, &nb_tests);
+    print_test_result(test_client__client_update_board(), &nb_success, &nb_tests);
+    print_test_result(test_client__client_play_card_success_case(), &nb_success, &nb_tests);
+
+    print_test_summary(nb_success, nb_tests);
+
+    return TEST_SUCCESS;
+}
