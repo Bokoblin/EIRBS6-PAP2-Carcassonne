a21573661f3bc7502fd07f453ab4a8300142fe07
broseau <broseau@enseirb-matmeca.fr>
2018-05-12 10:34:04 +0000

    Fixed an issue with the CMakeList.

--- cmake_build.sh
+++ cmake_build.sh
@@ -9,18 +9,6 @@ cmake ..
 make
 cd ..
 
-#Copying the executables in a bin repository
-mkdir -p bin
-mkdir -p bin/tst
-mkdir -p bin/clients
-cp build/*.so bin/clients
-cp build/server bin/
-cp build/tst/test_* bin/tst
-
-#Copying the executables where "make build" and 'make install" build it
+#Copying the executables where "make build" and "make install" build it
 cp build/*.so build/server install/
 cp build/tst/test_* ./
-
-#Creating a script to run the game from "bin" repository
-echo './server clients/*.so' > bin/run.sh
-echo built run.sh
--- cmake_clean.sh
+++ cmake_clean.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm -rf build
-rm -rf bin
 rm -rf doc
 make clean
